400ade4f16d141a9fdc026de938c40a77d038053
bcorrad <corradinibarbara0@gmail.com>
2025-04-11 18:48:53 +0200

    Update configuration parameters and enhance GIN model to support optional fingerprint processing

--- main.py
+++ main.py
@@ -23,6 +23,20 @@ MPL_VAL_PREC_LIST = []
 MPL_VAL_REC_LIST = []
 MPL_VAL_F1_LIST = []
 
+GAT_TRAIN_PREC_LIST = []
+GAT_TRAIN_REC_LIST = []
+GAT_TRAIN_F1_LIST = []
+GAT_VAL_PREC_LIST = []
+GAT_VAL_REC_LIST = []
+GAT_VAL_F1_LIST = []
+
+GATE_TRAIN_PREC_LIST = []
+GATE_TRAIN_REC_LIST = []
+GATE_TRAIN_F1_LIST = []
+GATE_VAL_PREC_LIST = []
+GATE_VAL_REC_LIST = []
+GATE_VAL_F1_LIST = []
+
 # Initialize a report to save the log of the training
 from config import EXPERIMENT_FOLDER, N_SAMPLES, RANDOMIZE_SAMPLES, USE_AVAILABLE_DATASET, BATCH_SIZE, CLS_LIST, PATHWAYS, LABELS_CODES, MULTILABEL2MULTICLASS, USE_FINGERPRINT
 import datetime
@@ -52,58 +66,90 @@ for MODEL in MODELS:
     print(f"Training {MODEL.upper()} model")
     f.write(f"Training {MODEL.upper()} model\n")
     
-    if MODEL == "gin" and gnn_train_dataloader is not None:
-        train_dataloader = gnn_train_dataloader
-        val_dataloader = gnn_val_dataloader
-        test_dataloader = gnn_test_dataloader
-        N_FEATURES = train_dataloader.dataset[0].x.shape[-1]
-        from models.GIN import GIN
-        model = GIN(num_node_features=N_FEATURES, 
-                    dim_h=H_DIM, 
-                    num_classes=len(LABELS_CODES.keys())).to(DEVICE) #, num_heads=4
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
-        loss_criterion = torch.nn.CrossEntropyLoss() if "hot" in TARGET_MODE or TARGET_MODE == "binary" else torch.nn.BCEWithLogitsLoss()  
-        from models.GIN import train_epoch
-        from models.GIN import evaluate   
-
-    elif MODEL == "gine" and gnn_train_dataloader is not None:
-        train_dataloader = gnn_train_dataloader
-        val_dataloader = gnn_val_dataloader
-        test_dataloader = gnn_test_dataloader
-        N_FEATURES = train_dataloader.dataset[0].x.shape[-1]
-        EDGE_FEATURES = train_dataloader.dataset[0].edge_attr.shape[-1]
-        if USE_FINGERPRINT and hasattr(train_dataloader.dataset[0], "fingerprint"):
-            FINGERPRINT_LENGTH = len(train_dataloader.dataset[0].fingerprint)
-        else:
-            FINGERPRINT_LENGTH = None
-
-        from models.GINE import GINWithEdgeFeatures
-        model = GINWithEdgeFeatures(in_channels=N_FEATURES, 
-                                    hidden_channels=H_DIM, 
-                                    edge_dim=EDGE_FEATURES, 
-                                    out_channels=len(LABELS_CODES.keys()),
-                                    fingerprint_length=FINGERPRINT_LENGTH)
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
-        loss_criterion = torch.nn.CrossEntropyLoss() 
-        from models.GINE import train_epoch
-        from models.GINE import evaluate
-        
-    elif MODEL == "mlp" and mlp_train_dataloader is not None:
-        train_dataloader = mlp_train_dataloader
-        val_dataloader = mlp_val_dataloader
-        test_dataloader = mlp_test_dataloader
-        from models.MLP import MLP
-        # N_FEATURES = the length of the extended fingerprint in the dataloader
-        N_FEATURES = len(train_dataloader.dataset[0][1][0])
-        # Create the model
-        model = MLP(input_channels=N_FEATURES, 
-                    num_categories=len(LABELS_CODES.keys())).to(DEVICE)
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
-        loss_criterion = torch.nn.BCELoss() if "hot" in TARGET_MODE or TARGET_MODE == "binary" else torch.nn.BCEWithLogitsLoss()
-        from models.MLP import train_epoch
-        from models.MLP import evaluate
-
     for n_run in range(N_RUNS):
+        
+        if MODEL == "gin" and gnn_train_dataloader is not None:
+            train_dataloader = gnn_train_dataloader
+            val_dataloader = gnn_val_dataloader
+            test_dataloader = gnn_test_dataloader
+            N_FEATURES = train_dataloader.dataset[0].x.shape[-1]
+            from models.GIN import GIN
+            model = GIN(num_node_features=N_FEATURES, 
+                        dim_h=H_DIM, 
+                        num_classes=len(LABELS_CODES.keys())).to(DEVICE) #, num_heads=4
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+            loss_criterion = torch.nn.CrossEntropyLoss() if "hot" in TARGET_MODE or TARGET_MODE == "binary" else torch.nn.BCEWithLogitsLoss()  
+            from models.GIN import train_epoch
+            from models.GIN import evaluate   
+
+        elif MODEL == "gine" and gnn_train_dataloader is not None:
+            train_dataloader = gnn_train_dataloader
+            val_dataloader = gnn_val_dataloader
+            test_dataloader = gnn_test_dataloader
+            N_FEATURES = train_dataloader.dataset[0].x.shape[-1]
+            EDGE_FEATURES = train_dataloader.dataset[0].edge_attr.shape[-1]
+            if USE_FINGERPRINT and hasattr(train_dataloader.dataset[0], "fingerprint"):
+                FINGERPRINT_LENGTH = len(train_dataloader.dataset[0].fingerprint)
+            else:
+                FINGERPRINT_LENGTH = None
+
+            from models.GINE import GINWithEdgeFeatures
+            model = GINWithEdgeFeatures(in_channels=N_FEATURES, 
+                                        hidden_channels=H_DIM, 
+                                        edge_dim=EDGE_FEATURES, 
+                                        out_channels=len(LABELS_CODES.keys()),
+                                        fingerprint_length=FINGERPRINT_LENGTH)
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+            loss_criterion = torch.nn.CrossEntropyLoss() 
+            from models.GINE import train_epoch
+            from models.GINE import evaluate
+            
+        elif MODEL == "gat" and gnn_train_dataloader is not None:
+            train_dataloader = gnn_train_dataloader
+            val_dataloader = gnn_val_dataloader
+            test_dataloader = gnn_test_dataloader
+            N_FEATURES = train_dataloader.dataset[0].x.shape[-1]
+            from models.GAT import GAT
+            model = GAT(in_channels=N_FEATURES, 
+                        hidden_channels=H_DIM, 
+                        out_channels=len(LABELS_CODES.keys())).to(DEVICE)
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+            loss_criterion = torch.nn.CrossEntropyLoss() 
+            from models.GAT import train_epoch
+            from models.GAT import evaluate
+            
+        elif MODEL == "gate" and gnn_train_dataloader is not None:
+            train_dataloader = gnn_train_dataloader
+            val_dataloader = gnn_val_dataloader
+            test_dataloader = gnn_test_dataloader
+            N_FEATURES = train_dataloader.dataset[0].x.shape[-1]
+            from models.GATE import GATE
+            model = GATE(in_channels=N_FEATURES, 
+                        hidden_channels=H_DIM, 
+                        out_channels=len(LABELS_CODES.keys()),
+                        edge_dim=EDGE_FEATURES).to(DEVICE)
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+            loss_criterion = torch.nn.CrossEntropyLoss() 
+            from models.GATE import train_epoch
+            from models.GATE import evaluate
+            
+        elif MODEL == "mlp" and mlp_train_dataloader is not None:
+            train_dataloader = mlp_train_dataloader
+            val_dataloader = mlp_val_dataloader
+            test_dataloader = mlp_test_dataloader
+            from models.MLP import MLP
+            # N_FEATURES = the length of the extended fingerprint in the dataloader
+            N_FEATURES = len(train_dataloader.dataset[0][1][0])
+            # Create the model
+            model = MLP(input_channels=N_FEATURES, 
+                        num_categories=len(LABELS_CODES.keys())).to(DEVICE)
+            optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+            loss_criterion = torch.nn.BCELoss() if "hot" in TARGET_MODE or TARGET_MODE == "binary" else torch.nn.BCEWithLogitsLoss()
+            from models.MLP import train_epoch
+            from models.MLP import evaluate
+
+        
+        
         for epoch in range(1, N_EPOCHS+1):
 
             train_avg_loss, train_precision, train_recall, train_f1, train_conf_matrix = train_epoch(model, train_dataloader, optimizer, loss_criterion, DEVICE, str(epoch))
@@ -125,6 +171,22 @@ for MODEL in MODELS:
                 GINE_VAL_REC_LIST.append(val_recall)
                 GINE_VAL_F1_LIST.append(val_f1)
                 
+            elif MODEL == "gat":
+                GAT_TRAIN_PREC_LIST.append(train_precision)
+                GAT_TRAIN_REC_LIST.append(train_recall)
+                GAT_TRAIN_F1_LIST.append(train_f1)
+                GAT_VAL_PREC_LIST.append(val_precision)
+                GAT_VAL_REC_LIST.append(val_recall)
+                GAT_VAL_F1_LIST.append(val_f1)
+                
+            elif MODEL == "gate":
+                GAT_TRAIN_PREC_LIST.append(train_precision)
+                GAT_TRAIN_REC_LIST.append(train_recall)
+                GAT_TRAIN_F1_LIST.append(train_f1)
+                GAT_VAL_PREC_LIST.append(val_precision)
+                GAT_VAL_REC_LIST.append(val_recall)
+                GAT_VAL_F1_LIST.append(val_f1)
+                
             elif MODEL == "mlp":
                 MPL_TRAIN_PREC_LIST.append(train_precision)
                 MPL_TRAIN_REC_LIST.append(train_recall)
@@ -133,14 +195,14 @@ for MODEL in MODELS:
                 MPL_VAL_REC_LIST.append(val_recall)
                 MPL_VAL_F1_LIST.append(val_f1)
                 
-            log_string = f'[{MODEL} TRAINING {n_run+1}/{N_RUNS}] Epoch: {epoch:03d}, Loss: {train_avg_loss:.4f}, Training Precision: {train_precision:.4f}, Training Recall: {train_recall:.4f}, Training F1-score: {train_f1:.4f}'
+            log_string = f'[{MODEL.upper()} TRAINING {n_run+1}/{N_RUNS}] Epoch: {epoch:03d}, Loss: {train_avg_loss:.4f}, Training Precision: {train_precision:.4f}, Training Recall: {train_recall:.4f}, Training F1-score: {train_f1:.4f}'
             f.write(log_string + "\n")
             print(log_string)
             if TARGET_TYPE == "pathway":
                 print("Training confusion matrix:") 
                 print(train_conf_matrix)
             
-            log_string = f'[{MODEL} VALIDATION {n_run+1}/{N_RUNS}] Epoch: {epoch:03d}, Loss: {val_avg_loss:.4f}, Val Precision: {val_precision:.4f}, Val Recall: {val_recall:.4f}, Val F1-score: {val_f1:.4f}'
+            log_string = f'[{MODEL.upper()} VALIDATION {n_run+1}/{N_RUNS}] Epoch: {epoch:03d}, Loss: {val_avg_loss:.4f}, Val Precision: {val_precision:.4f}, Val Recall: {val_recall:.4f}, Val F1-score: {val_f1:.4f}'
             f.write(log_string + "\n")
             print(log_string)
             if TARGET_TYPE == "pathway":
